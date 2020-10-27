0210b4625b88781e55068e34a011c0a871973cc2
Glen Larkin <glenlarkin@gmail.com>
2020-10-27 15:47:07 -0600

    Creating base app

---
+++ PokeDex/Pokemon.swift
@@ -0,0 +1,14 @@
+//
+//  Pokemon.swift
+//  PokeDex
+//
+//  Created by Glen Larkin on 10/27/20.
+//  Copyright © 2020 Glen Larkin. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon {
+    let name: String
+    let number: Int
+}
---
+++ PokeDex/PokemonViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PokemonViewController.swift
+//  PokeDex
+//
+//  Created by Glen Larkin on 10/27/20.
+//  Copyright © 2020 Glen Larkin. All rights reserved.
+//
+
+import Foundation
