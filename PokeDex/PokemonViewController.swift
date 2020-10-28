9117fc1b8756ed8433c44c471f467423b612da1f
Glen Larkin <glenlarkin@gmail.com>
2020-10-28 13:15:23 -0600

    Added PokemonViewController and segue to pass data from Pokemon Model

--- PokeDex/PokemonViewController.swift
+++ PokeDex/PokemonViewController.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2020 Glen Larkin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PokemonViewController: UIViewController {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var numberLabel: UILabel!
+    var pokemon: Pokemon!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLabel.text = pokemon.name
+        numberLabel.text = String(format: "#%03d", pokemon.number)
+    }
+    
+}
